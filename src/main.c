99bcd481114004988e8598c3ecc1517543874285
GermanSC <germanscillone@gmail.com>
2015-11-30 16:27:05 -0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Project	 : Server_Con1
+ Author      : German Sc.
+ Version     : 0.0
+ Copyright   : Completamente copyrighteado 2015
+ Description : Nada.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	/* Arrancamos */
+
+	printf("Arrancamos por hacer el Ejecutor de Comandos\n");
+
+	return 0;
+}
